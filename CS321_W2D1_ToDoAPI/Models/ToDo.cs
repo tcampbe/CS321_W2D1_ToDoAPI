bfa2ab5bc9d7d68e111f36984a64051c82ab27eb
Matt Cameron <cameron.matt@gmail.com>
2019-06-09 07:30:20 -0500

    initial commit

---
+++ CS321_W2D1_ToDoAPI/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS321_W2D1_ToDoAPI.Models
+{
+    public class ToDo
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80, ErrorMessage = "Description should be no more than 80 characters.")]
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public DateTime DueDate { get; set; }
+    }
+}
